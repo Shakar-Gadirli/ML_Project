ae7faf2ee9bc8f0b4a4d184e20983cf57ea5e087
Shakar-Gadirli <shakar.gadirli.std@bhos.edu.az>
2022-03-09 16:19:32 +0400

    db integration

--- project/app.py
+++ project/app.py
@@ -1,13 +1,32 @@
+import bson
 from flask import request, Flask, render_template, redirect
 from flask_pymongo import PyMongo
 
+import os
 import requests
 import validators
 from bs4 import BeautifulSoup
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = ""
+from pymongo.errors import DuplicateKeyError, OperationFailure
+from bson.objectid import ObjectId
+from bson.errors import InvalidId
+from werkzeug.utils import secure_filename
 
+app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://mlproject:mlproject@cluster0.5uzjt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongodb_client = PyMongo(app)
+db = mongodb_client.db
+
+'''
+def download_image(soup):
+	UPLOAD_FOLDER = "./images"
+	for image in soup.find_all("img"):
+		source =  image["src"]
+		img = Image.open(requests.get(image_url, stream = True).raw)
+		filename = secure_filename(img.filename)
+		img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		return path #bu image pathi goturub db-ya ataq
+'''		
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -29,6 +48,13 @@ def index():
             num_p_tags = len(tags_soup.find_all("p"))
             num_img_tags = len(tags_soup.find_all("img"))
             results = [num_p_tags, num_img_tags]
+			
+			
+			# download paragraphs function -> will return paragraphs that will be stored on the db
+			# download image function -> will return image path
+			# 
+			# after getting returns save results to db.
+
             return render_template("/pages/home.html", results=results)
 
 
